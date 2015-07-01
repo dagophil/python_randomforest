cef4b670587bda1effc73d0a90df0fb4a8394170
Philip Schill <philip.schill@gmx.de>
2015-07-01 12:30:12 +0200

    Number of jobs in random forest is now <= number of trees. Fixed label count in the nodes.

--- randomforest/randomforest_classifier.py
+++ randomforest/randomforest_classifier.py
@@ -40,7 +40,7 @@ class DecisionTreeClassifier(object):
         n = min(n, sh[1])
         return n
 
-    def fit(self, data, labels):
+    def fit(self, data, labels, bootstrap_sampling=False):
         """
         Train a decision tree.
 
@@ -60,9 +60,13 @@ class DecisionTreeClassifier(object):
 
         # Create the index vector.
         instances = numpy.array(xrange(0, data.shape[0]))
+        # if bootstrap_sampling:
+        #     sample_instances = numpy.random.random_integers(0, data.shape[0]-1, data.shape[0])
+        # else:
+        #     sample_instances = numpy.array(instances)
 
         # Add the root node to the graph and the queue.
-        self._graph.add_node(0, begin=0, end=data.shape[0], label_names=label_names, label_count=label_counts)
+        self._graph.add_node(0, begin=0, end=data.shape[0], num_labels=len(label_names), label_counts=label_counts)
         next_node_id = 1
         qu = collections.deque()
         qu.append(0)
@@ -74,7 +78,7 @@ class DecisionTreeClassifier(object):
             begin = node["begin"]
             end = node["end"]
             node_instances = instances[begin:end]
-            label_priors = node["label_count"]
+            label_priors = node["label_counts"]
 
             # Find the best split.
             best_gini = -1  # gini is always >= 0, so we can use -1 as "not set" value
@@ -113,12 +117,17 @@ class DecisionTreeClassifier(object):
             cl_left, counts_left = numpy.unique(labels_left, return_counts=True)
             cl_right, counts_right = numpy.unique(labels_right, return_counts=True)
 
+            class_count_left = numpy.zeros((len(self._label_names),), dtype=numpy.int_)
+            class_count_left[cl_left] = counts_left
+            class_count_right = numpy.zeros((len(self._label_names),), dtype=numpy.int_)
+            class_count_right[cl_right] = counts_right
+
             # Add the children to the graph.
             self._graph.add_node(next_node_id, begin=begin, end=middle,
-                                 label_names=cl_left, label_count=counts_left,
+                                 num_labels=len(cl_left), label_counts=class_count_left,
                                  is_left=True)
             self._graph.add_node(next_node_id+1, begin=middle, end=end,
-                                 label_names=cl_right, label_count=counts_right,
+                                 num_labels=len(cl_right), label_counts=class_count_right,
                                  is_left=False)
             self._graph.add_edge(node_id, next_node_id)
             self._graph.add_edge(node_id, next_node_id+1)
@@ -148,8 +157,8 @@ class DecisionTreeClassifier(object):
         node_label_count = numpy.zeros((num_nodes, len(self._label_names)), numpy.int_)
         for node_id in self._graph.nodes():
             node = self._graph.node[node_id]
-            for k, j in enumerate(node["label_names"]):
-                node_label_count[node_id, j] = node["label_count"][k]
+            for j, c in enumerate(node["label_counts"]):
+                node_label_count[node_id, j] = c
             if "split_dim" in node:
                 n0_id, n1_id = self._graph.neighbors(node_id)
                 if not self._graph.node[n0_id]["is_left"]:
@@ -159,8 +168,6 @@ class DecisionTreeClassifier(object):
                 node_children[node_id, 1] = n1_id
                 node_split_dims[node_id] = node["split_dim"]
                 node_split_values[node_id] = node["split_value"]
-            # else:
-            #     print node_label_count[node_id]
 
         # Call the cython probability function.
         probs = randomforest_functions.predict_proba(data.astype(numpy.float_), node_children, node_split_dims,
@@ -179,26 +186,36 @@ class DecisionTreeClassifier(object):
         return self._label_names[pred]
 
 
-def train_single_tree(tree, (data_ptr, data_dtype, data_shape), (labels_ptr, labels_dtype, labels_shape)):
+def train_single_tree(tree, (data_ptr, data_dtype, data_shape), (labels_ptr, labels_dtype, labels_shape),
+                      *args, **kwargs):
     """
     Train a single tree and return it.
 
     :param tree: the tree
+    :param data_ptr: the c_ptr to the data array (can be obtained using arr.ctypes.data)
+    :param data_dtype: the dtype of the data array
+    :param data_shape: the shape of the data array
+    :param labels_ptr: the c_ptr to the labels array (can be obtained using arr.ctypes.data)
+    :param labels_dtype: the dtype of the labels array
+    :param labels_shape: the shape of the labels array
     :return: the (trained) tree
     """
+    # Create the numpy array from data_ptr.
     data_size = 1
     for s in data_shape:
         data_size *= s
     data_bfr = numpy.core.multiarray.int_asbuffer(data_ptr, data_size * data_dtype.itemsize)
     data = numpy.frombuffer(data_bfr, data_dtype).reshape(data_shape)
 
+    # Create the labels array from labels_ptr.
     labels_size = 1
     for s in labels_shape:
         labels_size *= s
     labels_bfr = numpy.core.multiarray.int_asbuffer(labels_ptr, labels_size * labels_dtype.itemsize)
     labels = numpy.frombuffer(labels_bfr, labels_dtype).reshape(labels_shape)
 
-    tree.fit(data, labels)
+    # Call the tree.fit function.
+    tree.fit(data, labels, *args, **kwargs)
     return tree
 
 
@@ -210,7 +227,10 @@ class RandomForestClassifier(object):
         else:
             tree_rand_dims = "all"
 
-        self._n_estimators = n_estimators
+        if n_jobs is None:
+            n_jobs = multiprocessing.cpu_count()
+        assert n_jobs > 0
+
         self._n_jobs = n_jobs
         self._trees = [DecisionTreeClassifier(n_rand_dims=tree_rand_dims) for _ in xrange(n_estimators)]
         self._label_names = None
@@ -222,13 +242,14 @@ class RandomForestClassifier(object):
         :param data: the data
         :param labels: classes of the data
         """
-        if self._n_jobs == 1 or multiprocessing.cpu_count() == 1:
+        n_jobs = min(self._n_jobs, len(self._trees))
+        if n_jobs == 1:
             for tree in self._trees:
                 tree.fit(data, labels)
         else:
             data_info = (data.ctypes.data, data.dtype, data.shape)
             labels_info = (labels.ctypes.data, labels.dtype, labels.shape)
-            with concurrent.futures.ProcessPoolExecutor(self._n_jobs) as executor:
+            with concurrent.futures.ProcessPoolExecutor(n_jobs) as executor:
                 futures = []
                 for i, tree in enumerate(self._trees):
                     futures.append((i, executor.submit(train_single_tree, tree, data_info, labels_info)))
